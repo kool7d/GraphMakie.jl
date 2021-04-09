33071123cf37ea686bf4459e1166911f22a090b1
Documenter.jl <documenter@juliadocs.github.io>
2021-04-09 13:55:47 +0000

    build based on 057e2ef

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = GraphMakie","category":"page"},{"location":"#GraphMakie","page":"Home","title":"GraphMakie","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for GraphMakie.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [GraphMakie]","category":"page"}]
+}
